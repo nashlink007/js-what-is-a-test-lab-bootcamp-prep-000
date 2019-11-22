fd30c25263d2e7a14f1c80d3f5a5c01d81a250fa
Avinash Nepal <nepalavinash@gmail.com>
2019-11-22 18:14:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var name = "Susan"
 
+<<<<<<< HEAD
 var height = 39
 var message = `${name} is ${height} inches tall`
 module.exports = {name,height,message}
+=======
+var height = 74
+var message = ` is ${height} inches tall`
+var height = 40
+>>>>>>> 7cd6cda57f8dca28b67110a4862ad039daf30c00
